764ad298522a6ef4e75f981946b71be9bfef0288
HenriqueRamosdaSilva <146762258+HenriqueRamosdaSilva@users.noreply.github.com>
2025-08-20 19:02:17 -0300

    Add files via upload

---
+++ Aula01/Exercício04/BibliotecaFinanceiro/BibliotecaFinanceiro/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace BibliotecaFinanceiro
+{
+    public class Financeira
+    {
+        public double Juros_Compostos(double saldo_inicial, double taxa_juros, int tempo_rendendo)
+        {
+            return saldo_inicial * Math.Pow(1 + taxa_juros, tempo_rendendo);
+        }
+
+    }
+}
