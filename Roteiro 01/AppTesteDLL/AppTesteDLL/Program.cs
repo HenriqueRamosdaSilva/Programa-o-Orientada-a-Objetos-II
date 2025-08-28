7a0e966b12915380d0c6424949b40db88220bf35
HenriqueRamosdaSilva <146762258+HenriqueRamosdaSilva@users.noreply.github.com>
2025-08-28 20:58:52 -0300

    Add files via upload

---
+++ Roteiro 01/AppTesteDLL/AppTesteDLL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MinhaBiblioteca; // Namespace da DLL
+namespace AppTesteDLL
+{
+    class Program
+    {
+        static void Main()
+        {
+            var calc = new Calculadora();
+            Console.WriteLine("Soma: " + calc.Somar(5, 3));
+            Console.WriteLine("Multiplicação: " +
+           calc.Multiplicar(4, 2));
+        }
+    }
+}
