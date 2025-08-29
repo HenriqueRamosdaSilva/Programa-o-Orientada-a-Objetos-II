7927a8033cf8cffcf84feb5a0990f95999dd1b1c
HenriqueRamosdaSilva <146762258+HenriqueRamosdaSilva@users.noreply.github.com>
2025-08-28 21:29:57 -0300

    Add files via upload

---
+++ Roteiro 01/Exercicio 5/Exercicio 05 teste da dll do marcelo/Exercicio 05 teste da dll do marcelo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using BibliotecaEstatistica;
+
+namespace teste_da_dll_do_marcelo
+{
+    class Program
+    {
+        public double CalcularSoma(double[] numeros)
+        {
+            if (numeros == null)
+                return 0;
+            return numeros.Sum();
+        }
+
+        static void Main(string[] args)
+        {
+            double[] numeros = { 1.5, 2.5, 3.0, 4.0 };
+
+            Program estatistica = new Program(); // instanciando a própria classe Program
+            double soma = estatistica.CalcularSoma(numeros);
+
+            Console.WriteLine($"A soma dos números é: {soma}");
+        }
+    }
+}
