dee41691981b31943e7907341bb041f72a689374
HenriqueRamosdaSilva <146762258+HenriqueRamosdaSilva@users.noreply.github.com>
2025-09-11 21:53:16 -0300

    Add files via upload

---
+++ TBL 02/Exercicio 04/Exercicio 04/Exercicio 04/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio_04
+{
+    public class DivisaoPorZeroException : Exception
+    {
+        public DivisaoPorZeroException() : base("Divisão por zero não é permitida.")
+        {
+        }
+        public DivisaoPorZeroException(string message) : base(message)
+        {
+        }
+        public DivisaoPorZeroException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+
+    }
+
+
+   
+}
---
+++ TBL 02/Exercicio 04/Testando/Teste Ex04/Teste Ex04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Exercicio_04;
+
+namespace Teste_Ex04
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            try
+            {
+                Console.Write("Digite o numerador: ");
+                int numerador = int.Parse(Console.ReadLine());
+                Console.Write("Digite o denominador: ");
+                int denominador = int.Parse(Console.ReadLine());
+                if (denominador == 0)
+                {
+                    throw new DivisaoPorZeroException();
+                }
+                int resultado = numerador / denominador;
+                Console.WriteLine($"Resultado: {resultado}");
+            }
+            catch (DivisaoPorZeroException ex)
+            {
+                Console.WriteLine($"Erro personalizado: {ex.Message}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Erro de formato: Por favor, digite apenas números inteiros.");
+            }
+        }
+    }
+
+}
