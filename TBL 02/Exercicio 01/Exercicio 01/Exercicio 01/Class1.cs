8d2ae6bd289b46033cb7d8a587c0a1ad20f963a9
HenriqueRamosdaSilva <146762258+HenriqueRamosdaSilva@users.noreply.github.com>
2025-09-11 21:00:53 -0300

    Add files via upload

---
+++ TBL 02/Exercicio 01/Exercicio 01/Exercicio 01/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercicio_01
+{
+    public class IdadeInvalidaException : Exception
+    {
+        public IdadeInvalidaException(string message) : base(message)
+        {
+        }
+        public IdadeInvalidaException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
+}
+
---
+++ TBL 02/Testando/teste/teste/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Exercicio_01;
+
+using System;
+using Exercicio_01; // Importa a DLL (namespace onde está a exceção)
+
+namespace ProjetoTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestarIdade(-5);      
+            TestarIdade(130);     
+            TestarIdade(30);     
+        }
+
+        static void TestarIdade(int idade)
+        {
+            try
+            {
+                Console.WriteLine($"\nTestando idade: {idade}");
+                if (idade < 0 || idade > 120)
+                    throw new IdadeInvalidaException($"Idade inválida: {idade}");
+                Console.WriteLine("Idade válida! Cadastro permitido.");
+            }
+            catch (IdadeInvalidaException ex)
+            {
+                Console.WriteLine("⚠ Exceção capturada:");
+                Console.WriteLine($"Mensagem: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
+
