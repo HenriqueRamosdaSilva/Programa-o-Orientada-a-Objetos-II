7ae6bc319375fa1ad628fa39839936dfc2ae8ee6
HenriqueRamosdaSilva <146762258+HenriqueRamosdaSilva@users.noreply.github.com>
2025-09-11 21:52:27 -0300

    Add files via upload

---
+++ TBL 02/Exercicio 01/Testando/teste/teste/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Exercicio_01;
+namespace ProjetoTeste
+{
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            TestarIdade(-5);      
+            TestarIdade(130);     
+            TestarIdade(30);     
+        }
+        static void TestarIdade(int idade)
+        {
+            try
+            {
+                Console.WriteLine($"\nTestando idade: {idade}");
+                if (idade < 0 || idade > 120)
+                    throw new IdadeInvalidaException($"Idade inválida: {idade}");
+                Console.WriteLine("Idade válida! Cadastro permitido.");
+            }
+            catch (IdadeInvalidaException ex)
+            {
+                Console.WriteLine("⚠ Exceção capturada:");
+                Console.WriteLine($"Mensagem: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
+
