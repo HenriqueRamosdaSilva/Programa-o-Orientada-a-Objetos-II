f42e0f97f132c2df318a1f187a8ad4c5d8f7550d
HenriqueRamosdaSilva <146762258+HenriqueRamosdaSilva@users.noreply.github.com>
2025-10-02 22:31:14 -0300

    Add files via upload

---
+++ Roteiro JSON/Serialização Simples/Serialização Simples/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+class Program
+{
+    static void Main()
+    {
+        var livro = new Livro
+        {
+            Titulo = "Json o Primeiro Progrema",
+            Autor = "Henrique Ramos da Silva",
+            Ano = 2025
+        };
+        string json = JsonConvert.SerializeObject(livro, Formatting.Indented);
+        Console.WriteLine(json);
+
+
+    }
+}
+
+class Livro
+{
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public int Ano { get; set; }
+}
---
+++ Roteiro JSON/Uso de JObject/Uso de JObject/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+
+string json = @"
+{
+    'Servidor': 'Henriquecasa',
+    'Porta': 1433,
+    'Usuario': 'Henrique'
+}";
+
+JObject ser = JObject.Parse(json);
+
+
+Console.WriteLine(ser["Servidor"]); 
+Console.WriteLine(ser["Porta"]);
+Console.WriteLine(ser["Usuario"]);
+ser["Porta"] = 1111;
+Console.WriteLine(ser.ToString());
+
