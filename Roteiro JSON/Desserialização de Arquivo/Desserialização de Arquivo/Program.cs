7c03796699973507e5c0caf4102e1da0197574b4
HenriqueRamosdaSilva <146762258+HenriqueRamosdaSilva@users.noreply.github.com>
2025-10-02 22:29:39 -0300

    Add files via upload

---
+++ Roteiro JSON/Desserialização de Arquivo/Desserialização de Arquivo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main()
+    {
+        string json = @"{ 'Nome': 'Henrique', 'Idade': 21, 'Curso': 'Sistemas de Informação'}";
+        var Aluno = JsonConvert.DeserializeObject<Aluno>(json);
+        Console.WriteLine($"Nome: {Aluno.Nome}, Idade: {Aluno.Idade}, Curso: {Aluno.Curso}");
+
+    }
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Curso { get; set; }
+    }
+}
---
+++ Roteiro JSON/Lista de Objetos/Lista de Objetos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+        var carros = new List<Carro>
+        {
+            new Carro { Marca = "Toyota", Modelo = "Corolla", Ano = 2020 },
+            new Carro { Marca = "Honda", Modelo = "Civic", Ano = 2019 },
+            new Carro { Marca = "Byd", Modelo = "Não lembro", Ano = 2025 }
+        };
+string json = JsonConvert.SerializeObject(carros, Formatting.Indented);
+File.WriteAllText("carros.json", json);
+Console.WriteLine("Arquivo salvo com sucesso!");
+
+string jsonLido = File.ReadAllText("carros.json");
+var carrosLidos = JsonConvert.DeserializeObject<List<Carro>>(jsonLido);
+foreach (var carro in carrosLidos)
+{
+    Console.WriteLine($"Marca: {carro.Marca}, Modelo: {carro.Modelo}, Ano: {carro.Ano}");
+}
+
+
+
+class Carro
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public int Ano { get; set; }
+}
