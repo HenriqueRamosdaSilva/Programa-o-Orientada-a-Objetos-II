129e01f9639807e37794cb875a430cc5de11455e
HenriqueRamosdaSilva <146762258+HenriqueRamosdaSilva@users.noreply.github.com>
2025-10-16 22:21:49 -0300

    Add files via upload

---
+++ Roteiro XML/Exemplo/Exemplo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml;
+class Program
+{
+    static void Main()
+    {
+        using (XmlWriter writer = XmlWriter.Create("pedido.xml"))
+        {
+            writer.WriteStartDocument();
+            writer.WriteStartElement("Pedido");
+            writer.WriteStartElement("Item");
+            writer.WriteAttributeString("codigo", "A123");
+            writer.WriteAttributeString("quantidade", "10");
+            writer.WriteElementString("Descricao", "Cabo HDMI");
+            writer.WriteElementString("Preco", "49.90");
+            writer.WriteEndElement(); // Fecha Item
+            writer.WriteEndElement(); // Fecha Pedido
+            writer.WriteEndDocument();
+        }
+
+        Console.WriteLine("✅ Arquivo XML 'produto.xml' criado comsucesso!");
+    }
+}
---
+++ Roteiro XML/Exercício_1/Exercício_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+[XmlRoot("Aluno")]
+public class Aluno
+{
+    [XmlElement("Nome")]
+    public string Nome { get; set; }
+    [XmlElement("Curso")]
+    public string Curso { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var turma = new Turma
+        {
+            Alunos = new List<Aluno>
+             {
+                new Aluno { Nome = "Maria", Curso = "Sistemas de Informação" },
+                new Aluno { Nome = "João", Curso = "Engenharia de Software" }             }
+        };
+
+
+        XmlSerializer serializer = new XmlSerializer(typeof(Turma));
+        using (StreamWriter writer = new StreamWriter("turma.xml"))
+        {
+            serializer.Serialize(writer, turma);
+        }
+
+        using (StreamReader reader = new StreamReader("turma.xml"))
+        {
+            Turma turmaDeserializada = (Turma)serializer.Deserialize(reader);
+
+            foreach (var aluno in turmaDeserializada.Alunos)
+            {
+                Console.WriteLine($"Nome: {aluno.Nome}");
+                Console.WriteLine($"Curso: {aluno.Curso}");
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
+[XmlRoot("Turma")]
+public class Turma
+{
+    [XmlArray("Alunos")]
+    [XmlArrayItem("Aluno")]
+    public List<Aluno> Alunos { get; set; }
+}
